edda9649679d4101ba5235c8c5bb9989f9cad595
Franz-Raven <franzravensanchez@gmail.com>
2025-09-26 02:48:20 +0800

    feature(register): added the register feature

--- mycebu_app/urls.py
+++ mycebu_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name="login"),
+    path('register/', views.register_view, name="register"),
+    path('login/', views.login_view, name="login"),
     # path('logout/', LogoutView.as_view(), name='logout'),
     # path('register/', views.register, name='register'),
     # path('dashboard/', views.dashboard, name='dashboard'),
--- mycebu_app/views.py
+++ mycebu_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login_view(request):
     return render(request, 'mycebu_app/login.html')
+
+def register_view(request):
+    return render(request, 'mycebu_app/register.html')
