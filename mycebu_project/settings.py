59edde3dd1942586068152d8c1286625454e3377
MRX <mrxmrx459@gmail.com>
2025-09-28 17:22:01 +0800

    fix(static): restore previous static configuration pending proper deployment setup

--- mycebu_project/settings.py
+++ mycebu_project/settings.py
@@ -124,14 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')   # Your existing static folder
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic will store files
-
+STATIC_URL = 'static/mycebu_app/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
