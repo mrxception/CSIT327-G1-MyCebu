f4a2faa068f10e7f1cdf8d2716294768ce08db6b
MRX <mrxmrx459@gmail.com>
2025-10-15 07:27:11 +0800

    feature(auth): add error message below specific input

--- mycebu_app/urls.py
+++ mycebu_app/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.landing_view, name='landing'),
-    #path('landing/', views.landing_view, name='landing'),
     path('register/', views.register_view, name="register"),
     path('login/', views.login_view, name="login"),
     path('test/', views.chatbot_page, name='chatbot_page'),
@@ -13,9 +11,11 @@ urlpatterns = [
     path('password-reset-new-password/', views.password_reset_new_password_view, name='password_reset_new_password'),
     path('password-reset-success/', views.password_reset_success_view, name='password_reset_success'),
 
+    path('logout/', views.logout_view, name='logout'),
+
+    # Root landing
     path('', views.landing_view, {'tab': 'landing'}, name='landing_default'),
+    
+    # Catch-all for tabs (logout won't reach here now)
     path('<str:tab>/', views.landing_view, name='landing_tab'), 
-    # path('logout/', LogoutView.as_view(), name='logout'),
-    # path('register/', views.register, name='register'),
-    # path('dashboard/', views.dashboard, name='dashboard'),
 ]
