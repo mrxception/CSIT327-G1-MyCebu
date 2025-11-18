2cf1a70eb17278752ae7d04b3a33356f1d69dfde
MRX <mrxmrx459@gmail.com>
2025-11-18 22:12:47 +0800

    feature(service): add service pages

--- mycebu_app/urls.py
+++ mycebu_app/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('landing/', views.landing_view, {'tab': 'landing'}, name='landing_default'),
     path('<str:tab>/', views.landing_view, name='landing_tab'),
 
-    path("apply/business-permit/", views.apply_business_permit_view, name="apply_business_permit"),
-    path("apply/business-permit/start/", views.start_business_permit_application, name="start_business_permit"),
-    path("apply/business-permit/<uuid:app_id>/", views.permit_progress_view, name="permit_progress"),
-    path("apply/business-permit/<uuid:app_id>/update/", views.update_business_permit_step, name="update_business_permit"),
+    path("apply/<str:service>/", views.apply_permit_view, name="apply_permit"),
+    path("apply/<str:service>/start/", views.start_service_application, name="start_service_application"),
+    path("apply/<str:service>/<uuid:app_id>/", views.permit_progress_view, name="permit_progress"),
+    path("apply/<str:service>/<uuid:app_id>/update/", views.update_service_application, name="update_service_application"),
 
 ]
