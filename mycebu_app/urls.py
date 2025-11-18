40c7241fee1f9c3d1e02f2ff3ae54b1cc3cb5fe1
MRX <mrxmrx459@gmail.com>
2025-11-18 21:42:53 +0800

    feature(services): add apply for business permit page

--- mycebu_app/urls.py
+++ mycebu_app/urls.py
@@ -14,4 +14,10 @@ urlpatterns = [
     path('dashboard/', views.landing_view, {'tab': 'dashboard'}, name='dashboard'),
     path('landing/', views.landing_view, {'tab': 'landing'}, name='landing_default'),
     path('<str:tab>/', views.landing_view, name='landing_tab'),
+
+    path("apply/business-permit/", views.apply_business_permit_view, name="apply_business_permit"),
+    path("apply/business-permit/start/", views.start_business_permit_application, name="start_business_permit"),
+    path("apply/business-permit/<uuid:app_id>/", views.permit_progress_view, name="permit_progress"),
+    path("apply/business-permit/<uuid:app_id>/update/", views.update_business_permit_step, name="update_business_permit"),
+
 ]
